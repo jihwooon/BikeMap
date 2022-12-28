d4b31be84bf1a78f6df8604908d6b864e6e2dc47
jihwooon <jihwooon@gmail.com>
2022-12-28 20:52:30 +0900

    RefreshToken을 생성하라
    RefreshToken 토큰 생성하는 함수를 생성했습니다.

--- kotlin-server/src/test/kotlin/com/bikemap/api/jwt/JwtUtilTest.kt
+++ kotlin-server/src/test/kotlin/com/bikemap/api/jwt/JwtUtilTest.kt
@@ -5,18 +5,28 @@ import io.kotest.matchers.shouldBe
 
 class JwtUtilTest : DescribeSpec() {
 
-    private val SECRET = "9l3hgt08wgcljwwx7itch6z1nwe236w3"
+    private val ACCESS_TOKEN_SECRET = "9l3hgt08wgcljwwx7itch6z1nwe236w3"
+    private val REFRESH_TOKEN_SECRET = "9l3hgt08wgcljwwx7itch6z1nwe23123"
     private val ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MX0.vMou9TCv70zWW2SGzPVB2R7go07DYfX_CKTyX2UEfDM"
-    private val jwtUtil = JwtUtil(SECRET)
+    private val REFRESH_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MX0.KCWqyeQwpjw1z7bwG0yuPx4rTyRtYkPc6Pj0jYugmlM"
+
+    private val jwtUtil = JwtUtil(ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET)
 
     init {
         describe("JwtUtil class") {
             val userId = 1L
-            context("secret 정보가 주어지면") {
-                it("accessToken를 반환한다.") {
+            context("accessSecret 정보가 주어지면") {
+                it("accessToken을 반환한다.") {
                     val accessToken = jwtUtil.generateAccessToken(userId)
 
                     accessToken.shouldBe(ACCESS_TOKEN)
+                }
+            }
+            context("refreshSecret 정보가 주어지면") {
+                it("refreshToken을 반환한다.") {
+                    val refreshToken = jwtUtil.generateRefreshToken(userId)
+
+                    refreshToken.shouldBe(REFRESH_TOKEN)
                 }
             }
         }
