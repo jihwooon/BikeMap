c1182b9c508cc89cb71861ba6e200f936e630100
jihwooon <jihwooon@gmail.com>
2023-01-05 20:42:39 +0900

    LoginController를 SigninController로 변경하라
    로그인을 Login으로 표현하는 것보다 Signin으로 표현으로 변경하는게 더 낫다고 생각해서 변경했습니다.

---
+++ kotlin-server/src/test/kotlin/com/bikemap/api/web/signin/SigninControllerTest.kt
@@ -0,0 +1,47 @@
+package com.bikemap.api.web.signin
+
+import com.bikemap.api.application.auth.domain.Authentication
+import com.bikemap.api.application.auth.service.SigninService
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.ninjasquad.springmockk.MockkBean
+import io.kotest.core.spec.style.DescribeSpec
+import io.mockk.every
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
+
+@WebMvcTest(SigninController::class)
+class SigninControllerTest(mockMvc: MockMvc) : DescribeSpec() {
+    val objectMapper = ObjectMapper().registerModule(KotlinModule())
+
+    @MockkBean
+    private lateinit var signinService: SigninService
+
+    val signinData =
+        SigninRequest("test@email.com", "1234")
+
+    init {
+        beforeEach {
+            every {
+                signinService.signin(signinData.email, signinData.password)
+            } returns Authentication("", "")
+        }
+
+        describe("SigninController") {
+            context("Post /signin을 요청하면") {
+                it("signin을 반환한다.") {
+                    mockMvc.post("/signin") {
+                        contentType = MediaType.APPLICATION_JSON
+                        content = objectMapper.writeValueAsString(signinData)
+                    }.andExpect {
+                        status { isCreated() }
+                        jsonPath("$.accessToken") { isString() }
+                        jsonPath("$.refreshToken") { isString() }
+                    }
+                }
+            }
+        }
+    }
+}
