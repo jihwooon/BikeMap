ec9bf05a7c0c40c6c6350d4a17249d5a1d276028
jihwooon <jihwooon@gmail.com>
2023-01-20 11:48:59 +0900

    UserTest 테스트코드를 수정하라
    기존 String 타입의 값들을 객체타입으로 변경되어서 테스트코드도 맞게 수정했습니다.

--- kotlin-server/src/test/kotlin/com/bikemap/api/application/user/domain/UserTest.kt
+++ kotlin-server/src/test/kotlin/com/bikemap/api/application/user/domain/UserTest.kt
@@ -3,25 +3,26 @@ package com.bikemap.api.application.user.domain
 import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import java.time.LocalDate
 
 class UserTest : StringSpec({
 
     "회원 정보를 확인한다" {
         val id = 1L
         val username = "홍길동"
-        val email = "test@email.com"
+        val email = Email("test@email.com")
         val password = Password("bikemap12345@@", "ugSmbgmN6xpyeV6RtY6NGQ==")
-        val gender = "남성"
-        val birth = "1999-01-01"
+        val gender = Gender.MALE
+        val birth = LocalDate.of(1990, 1, 1)
         val user = User(id, username, email, password, gender, birth)
 
         assertSoftly(user) {
             user.id shouldBe 1
             user.username shouldBe "홍길동"
-            user.email shouldBe "test@email.com"
+            user.email shouldBe email
             user.password shouldBe password
-            user.gender shouldBe "남성"
-            user.birth shouldBe "1999-01-01"
+            user.gender shouldBe gender
+            user.birth shouldBe birth
         }
     }
 })
