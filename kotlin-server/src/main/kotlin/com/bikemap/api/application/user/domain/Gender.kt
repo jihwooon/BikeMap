8c9b4178e359694807d69da2079ed4ea935e09bf
jihwooon <jihwooon@gmail.com>
2023-01-20 11:48:16 +0900

    String 타입의 객체 필드를 객체 타입으로 변경하라

---
+++ kotlin-server/src/main/kotlin/com/bikemap/api/application/user/domain/Gender.kt
@@ -0,0 +1,6 @@
+package com.bikemap.api.application.user.domain
+
+enum class Gender(val title: String) {
+    MALE("남성"),
+    FEMALE("여성")
+}
--- kotlin-server/src/main/kotlin/com/bikemap/api/application/user/domain/User.kt
+++ kotlin-server/src/main/kotlin/com/bikemap/api/application/user/domain/User.kt
@@ -1,26 +1,37 @@
 package com.bikemap.api.application.user.domain
 
+import jakarta.persistence.Column
 import jakarta.persistence.Embedded
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import java.time.LocalDate
 
 @Entity
 class User(
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0,
+    val id: Long = 0L,
 
+    @Column(nullable = false)
     val username: String = "",
 
-    val email: String = "",
+    @Column(nullable = false)
+    @Embedded
+    val email: Email = Email(),
 
+    @Column(nullable = false)
     @Embedded
     val password: Password = Password(),
 
-    val gender: String = "",
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    val gender: Gender = Gender.MALE,
 
-    val birth: String = "",
+    @Column(nullable = false)
+    val birth: LocalDate = LocalDate.now(),
 )
