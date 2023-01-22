97133aee6d02cb3b2a8a3e42a055c66ac0c1b5cd
jihwooon <jihwooon@gmail.com>
2023-01-23 00:01:00 +0900

    SignupController에 service를 추가하라
    SignupService를 추가했습니다.

--- kotlin-server/src/main/kotlin/com/bikemap/api/web/signup/SignupController.kt
+++ kotlin-server/src/main/kotlin/com/bikemap/api/web/signup/SignupController.kt
@@ -1,5 +1,6 @@
 package com.bikemap.api.web.signup
 
+import com.bikemap.api.application.auth.service.SignupService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RestController
  * 회원 가입 컨트롤러
  */
 @RestController
-class SignupController {
+class SignupController(
+    val signupService: SignupService
+) {
     /**
      * 회원 가입을 합니다.
      *
@@ -20,6 +23,8 @@ class SignupController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
     fun handleSignup(@Valid @RequestBody request: SignupRequest): SignupResponse {
-        return SignupResponse("", "")
+        val authentication =
+            signupService.signup(request.email, request.password, request.username, request.gender, request.birth)
+        return SignupResponse(authentication.accessToken, authentication.refreshToken)
     }
 }
